76665154b10427814898dd50ec14de0f2de6189a
danielholmes839 <danielh839@gmail.com>
2024-12-07 12:09:40 -0500

    day 7 part 2

--- scripts/day7.ex
+++ scripts/day7.ex
@@ -14,21 +14,29 @@ equations =
   end)
 
 defmodule Solver do
-  def solve({result, [operand | remaining_operands]}) do
-    solve({result, remaining_operands}, operand)
+  def solve({target, [operand | remaining_operands]}) do
+    # set the first operand as the acc
+    solve({target, remaining_operands}, operand)
   end
 
-  def solve({result, [operand | remaining_operands]}, acc) do
-    if acc > result do
+  def solve({target, []}, acc) do
+    # empty operands so check the acc
+    acc == target
+  end
+
+  def solve({target, [operand | remaining_operands]}, acc) do
+    if acc > target do
       false
     else
-      solve({result, remaining_operands}, acc * operand) or
-        solve({result, remaining_operands}, acc + operand)
+      solve({target, remaining_operands}, acc * operand) or
+        solve({target, remaining_operands}, acc + operand) or
+        solve({target, remaining_operands}, concat(acc, operand))
     end
   end
 
-  def solve({result, []}, acc) do
-    acc == result
+  def concat(target, acc) do
+    str = Integer.to_string(target) <> Integer.to_string(acc)
+    String.to_integer(str)
   end
 end
 
@@ -38,3 +46,6 @@ IO.inspect(
   |> Enum.map(fn {result, _} -> result end)
   |> Enum.sum()
 )
+
+# part 1 solution: 14711933466277
+# part 2 solution: 286580387663654
