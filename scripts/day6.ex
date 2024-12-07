396f6d042d33e075b986856413d62e0611073a22
danielholmes839 <danielh839@gmail.com>
2024-12-07 17:47:08 -0500

    cleaner day 6 part 1

--- scripts/day6.ex
+++ scripts/day6.ex
@@ -1,34 +1,33 @@
 {:ok, contents} = File.read("data/day6.txt")
 
 defmodule Explore do
-  def explore(%{} = explored, map, {x, y} = position, direction) do
+  def explore(%{} = explored, grid, {x, y} = position, direction) do
     directions = %{up: {0, -1}, right: {1, 0}, down: {0, 1}, left: {-1, 0}}
     transitions = %{up: :right, right: :down, down: :left, left: :up}
 
-    if Map.has_key?(explored, {x, y, direction}) or Map.get(map, position) == nil do
-      explored
-    else
-      {dx, dy} = Map.get(directions, direction)
-      explored = Map.put(explored, {x, y, direction}, true)
+    explored = Map.put(explored, {x, y, direction}, true)
+    {dx, dy} = Map.get(directions, direction)
 
-      new_direction =
-        case Map.get(map, {x + dx, y + dy}) do
-          "#" ->
-            Map.get(transitions, direction)
+    new_position = {x + dx, y + dy}
 
-          _ ->
-            direction
-        end
+    case Map.get(grid, new_position) do
+      nil ->
+        # reached the end of the grid
+        explored
 
-      {dx, dy} = Map.get(directions, new_direction)
-      new_position = {x + dx, y + dy}
+      "#" ->
+        # hit wall. explore the same position but new direction
+        new_direction = Map.get(transitions, direction)
+        explore(explored, grid, position, new_direction)
 
-      explore(explored, map, new_position, new_direction)
+      _ ->
+        # walk in current direction
+        explore(explored, grid, new_position, direction)
     end
   end
 end
 
-map =
+grid =
   contents
   |> String.split("\n")
   |> Enum.with_index()
@@ -39,19 +38,14 @@ map =
     |> Enum.map(fn {char, charNum} -> {char, charNum, lineNum} end)
   end)
   |> List.flatten()
-  |> Enum.reduce(%{}, fn {char, i, j}, map -> Map.put(map, {i, j}, char) end)
+  |> Enum.reduce(%{}, fn {char, i, j}, grid -> Map.put(grid, {i, j}, char) end)
 
-{pos_start, _} = Enum.find(map, fn {_, val} -> val == "^" end)
+{pos_start, _} = Enum.find(grid, fn {_, val} -> val == "^" end)
 
 unique =
-  Explore.explore(%{}, map, pos_start, :up)
+  Explore.explore(%{}, grid, pos_start, :up)
   |> Map.keys()
   |> Enum.map(fn {x, y, _} -> {x, y} end)
   |> Enum.uniq()
 
 IO.inspect(length(unique))
-
-# IO.inspect(map)
-# IO.inspect(start)
-# IO.inspect(directions)
-# IO.inspect(transitions)
