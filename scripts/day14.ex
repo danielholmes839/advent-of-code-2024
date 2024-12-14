4d1711a2be88b9fbd0375eb77e901e7af0144a46
danielholmes839 <danielh839@gmail.com>
2024-12-14 11:40:26 -0500

    day 14 part 2

--- scripts/day14.ex
+++ scripts/day14.ex
@@ -24,6 +24,23 @@ defmodule Day14 do
         :mid
     end
   end
+
+  def draw(robots, {width, height}) do
+    robots = robots |> Enum.group_by(fn pos -> pos end)
+
+    for y <- 0..(height - 1) do
+      row =
+        for x <- 0..(width - 1), into: "" do
+          if Map.has_key?(robots, {x, y}) do
+            "#"
+          else
+            " "
+          end
+        end
+
+      IO.puts(row)
+    end
+  end
 end
 
 {:ok, content} = File.read("data/day14.txt")
@@ -40,17 +57,29 @@ robots =
 
 dims = {101, 103}
 mid = {50, 51}
-seconds = 100
+# seconds = 100
 
-safety =
-  robots
-  |> Enum.map(fn {pos, vel} ->
-    Day14.final_position(pos, vel, dims, seconds)
-  end)
-  |> Enum.group_by(fn pos -> Day14.quadrant(pos, mid) end)
-  |> Map.delete(:mid)
-  |> Map.values()
-  |> Enum.map(&length/1)
-  |> Enum.reduce(1, fn len, acc -> acc * len end)
+1..100_000
+|> Enum.map(fn seconds ->
+  # IO.puts("-------------------")
 
-IO.inspect(safety)
+  robots =
+    robots
+    |> Enum.map(fn {pos, vel} ->
+      Day14.final_position(pos, vel, dims, seconds)
+    end)
+
+  if length(Enum.uniq(robots)) == 500 do
+    Day14.draw(robots, dims)
+    IO.puts(seconds)
+    exit("")
+  end
+end)
+
+# |> Enum.group_by(fn pos -> Day14.quadrant(pos, mid) end)
+# |> Map.delete(:mid)
+# |> Map.values()
+# |> Enum.map(&length/1)
+# |> Enum.reduce(1, fn len, acc -> acc * len end)
+
+# IO.inspect(safety)
